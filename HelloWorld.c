086d5c7d5eed4aa5805efca4fc8f4ff4ba29d290
EUNSU <dldmstn0510@naver.com>
2020-12-20 17:42:31 +0900

    add function in HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,24 @@
+//add
 #include <stdio.h>
+int add(int a, int b);
 
 int main(void)
 {
+	int a;
+	int b;
+	int c;
+
+	a = 5;
+	b = 10;
+	c = add(a, b);
+
+	
 	printf("hello, world!\n");
+	
 
 	return 0;
+}
+int add(int a, int b)
+{
+	return a + b;
 }
